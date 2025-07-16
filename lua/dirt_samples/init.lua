646dc7d1f014b8c234baf959d90796d1522475c6
Bss <mx.bss@proton.me>
2025-07-16 18:31:29 +0200

    working base extension

--- lua/dirt_samples/init.lua
+++ lua/dirt_samples/init.lua
@@ -6,28 +6,35 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-local source_path = vim.fn.expand("~/livecoding/supercollider/superdirt-setup/tmp/sounds.txt")
-local sample_names = vim.fn.readfile(source_path)
+local config = {
+	source_path = vim.fn.expand("~/livecoding/supercollider/superdirt-setup/tmp/sounds.txt"),
+}
 
-local names = function (opts)
-    opts = opts or {}
-    pickers.new(opts, {
-        prompt_title = "Dirt Samples",
-        finder = finders.new_table {
-            results = sample_names,
-        },
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function (prompt_bufnr, map)
-            actions.select_default:replace(function ()
-                actions.close(prompt_bufnr)
-                local selection = action_state.get_selected_entry()
-                vim.api.nvim_put({ selection[1] }, "", false, true)
-            end)
-            return true
-        end,
-    }):find()
+---@param opts? {source_path?: string}
+dirt_samples.setup = function(opts)
+	config = vim.tbl_deep_extend("force", config, opts or {})
 end
 
-names(require("telescope.themes").get_ivy({}))
+dirt_samples.names = function(opts)
+	opts = opts or {}
+	local sample_names = vim.fn.readfile(config.source_path)
+	pickers
+		.new(opts, {
+			prompt_title = "Dirt Samples",
+			finder = finders.new_table({
+				results = sample_names,
+			}),
+			sorter = conf.generic_sorter(opts),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					vim.api.nvim_put({ selection[1] }, "", false, true)
+				end)
+				return true
+			end,
+		})
+		:find()
+end
 
--- return dirt_samples
+return dirt_samples
