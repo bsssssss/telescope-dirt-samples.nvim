4e22ec3a2bfaccecacfbb871fd8e55f3faa2f408
Bss <mx.bss@proton.me>
2025-09-05 15:00:19 +0200

    format

--- lua/telescope/_extensions/dirt_samples.lua
+++ lua/telescope/_extensions/dirt_samples.lua
@@ -1,6 +1,5 @@
 return require("telescope").register_extension {
-    setup = function (ext_config, config)
-    end,
+    setup = function (ext_config, config) end,
     exports = {
         dirt_samples = require("dirt_samples").names
     }
